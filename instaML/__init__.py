507119289f3c66f43843805520f6baa4ea7769a7
root <root@localhost.localdomain>
2020-03-16 21:14:38 +0530

    few changes

--- instaML/__init__.py
+++ instaML/__init__.py
@@ -2,4 +2,4 @@
 __author__ = """Vikneshwar Selvam"""
 __email__ = 'selvamvikneshwar@gmail.com'
 
-from instaML.facial_recognition.facial_rec import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
+from instaML.facial_rec import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
