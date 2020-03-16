e432f0beeca11d9028d07c788cbdd8f3891aa22c
root <root@localhost.localdomain>
2020-03-16 20:51:00 +0530

    added release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Vikneshwar Selvam',                   # Type in your name
   author_email = 'selvamvikneshwar@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/vikneshwarselvam/instaML',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/vikneshwarselvam/instaML/archive/v1.tar.gz',    # I explain this later on
   keywords = ['Easy', 'Simple', 'Best'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
